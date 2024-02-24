f4bf9a0c4928011340fd8280488a6b1c41159559
amithkashyap <amithkashyaprv@gmail.com>
2024-02-25 00:34:21 +0530

    added loader and action for order and createOrder routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 function App() {
   const router = createBrowserRouter([
     {
       element: <AppLayout />,
+      errorElement: <Error />,
       children: [
         {
           path: "/",
@@ -32,10 +36,12 @@ function App() {
         {
           path: "/order/new",
           element: <CreateOrder />,
+          action: createOrderAction,
         },
         {
           path: "/order/:orderId",
           element: <Order />,
+          loader: orderLoader,
         },
       ],
     },
