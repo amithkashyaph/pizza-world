7ccd6e8e2803bfb8c4383597d76d575f9c248607
amithkashyap <amithkashyaprv@gmail.com>
2024-03-04 00:04:38 +0530

    added CartOverview component

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCartTotalAmount, getCartTotalQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
+  const cartTotalQuantity = useSelector(getCartTotalQuantity);
+  const cartTotalAmount = useSelector(getCartTotalAmount);
+  if (!cartTotalQuantity) {
+    return null;
+  }
   return (
-    <div>
-      <p>
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+    <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:p-6">
+      <p className="space-x-4 font-semibold text-stone-300">
+        <span>{cartTotalQuantity} Pizzas</span>
+        <span>{formatCurrency(cartTotalAmount)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
