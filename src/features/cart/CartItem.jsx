5479433f0f80f86a6f3e51b80d4bc6869f68e1b7
amithkashyap <amithkashyaprv@gmail.com>
2024-03-03 23:41:32 +0530

    added CartItem component

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,17 +1,19 @@
-import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import DeleteItem from '../../ui/DeleteItem';
+import UpdateItemQuantity from '../../ui/UpdateItemQuantity';
 
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { id, name, quantity, totalPrice } = item;
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
-        {quantity}&times; {name}
+        {quantity}&times; <span className="font-semibold">{name}</span>
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <UpdateItemQuantity id={id} quantity={quantity} />
+        <DeleteItem id={id} />
       </div>
     </li>
   );
