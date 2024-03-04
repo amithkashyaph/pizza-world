321e2505c108138fddf4e37f4d0902a1947846c7
amithkashyap <amithkashyaprv@gmail.com>
2024-03-04 19:40:33 +0530

    added EmptyCart component

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom';
 function EmptyCart() {
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link
+        to="/menu"
+        className="my-6 inline-block rounded-full bg-yellow-500 px-3 py-2 shadow-lg hover:bg-yellow-400"
+      >
+        &larr; Back to menu
+      </Link>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <p className="mt-10 text-center text-3xl">
+        Your cart is still empty. Start adding some pizzas :)
+      </p>
     </div>
   );
 }
