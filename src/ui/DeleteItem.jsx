2fd83febabeea10f543e4ef9f32b2864d74ae56e
amithkashyap <amithkashyaprv@gmail.com>
2024-03-10 03:30:14 +0530

    implemented Delete functionality

---
+++ src/ui/DeleteItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+import { useDispatch } from 'react-redux';
+import { deleteItem } from '../features/cart/cartSlice';
+
+const DeleteItem = ({ id }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Button type="small" onClick={() => dispatch(deleteItem(id))}>
+      Delete
+    </Button>
+  );
+};
+
+export default DeleteItem;
