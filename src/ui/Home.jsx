8c860da7274aaefbb564fe67121b4d9b038fe74a
amithkashyap <amithkashyaprv@gmail.com>
2024-03-11 02:07:51 +0530

    refactored to style Home component

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,25 @@
+import { useSelector } from 'react-redux';
+import CreateUser from '../features/user/CreateUser';
+import Button from './Button';
+
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
-    <div>
-      <h1>
+    <div className="my-10 text-center">
+      <h1 className="mb-6  text-xl font-semibold text-stone-700 sm:mb-8 md:text-3xl">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+      {username ? (
+        <Button type="primary" to="/menu">
+          Explore Menu, {username}
+        </Button>
+      ) : (
+        <CreateUser />
+      )}
     </div>
   );
 }
