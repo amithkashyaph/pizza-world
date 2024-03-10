e1833cd8a2b1e114ab9c7c39827dc79f2ec8584f
amithkashyap <amithkashyaprv@gmail.com>
2024-03-10 12:40:21 +0530

    refactored header component to add more links and styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Pizza World!</Link>
+    <header className="grow border-b border-stone-200 bg-yellow-500 px-4 py-3 text-center uppercase tracking-widest shadow-lg sm:flex sm:items-center sm:justify-between">
+      <Link
+        to="/"
+        className="flex items-center justify-center gap-2 font-roboto text-xl sm:text-2xl"
+      >
+        <img src="../../public/pizza.png" className="w-9" />
+        <span>Pizza World</span>
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 };
